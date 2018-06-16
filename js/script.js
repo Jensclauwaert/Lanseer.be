067a76b9d90103c960524b7cf442c9686eb3c699
jensclauwaert <jens.clauwaert@student.ap.be>
2018-06-17 00:01:57 +0200

    linking , content and css fixes

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function main(){
+
+var closed = document.getElementsByClassName("hamburger")[0];
+var opened = document.getElementsByClassName("hamburgerhid")[0];
+var secret = document.getElementsByClassName("hiddenNav")[0];
+
+closed.onclick = function(){
+ secret.style.height = "100vh";
+
+ console.log("test");
+ };
+
+ opened.onclick = function(){
+  secret.style.height = "0vh";
+
+  };
+
+
+  console.log('jensisking');
+}
+
+
+window.onload = function () {
+ main();
+};
